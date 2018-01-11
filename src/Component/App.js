64e2917e0cf8769729f3845884da6a07a41bb0b4
DC <heyyodc-git@yahoo.com>
2018-01-11 15:10:15 -0800

    lift state and actions from lower comp to root component

--- src/Component/App.js
+++ src/Component/App.js
@@ -4,14 +4,44 @@ import JumbotronFluid from "./elements/JumbotronFluid";
 import UserList from "./UserList";
 
 class App extends Component {
+	constructor() {
+		super();
+
+		// Initialize users in state as an empty array and
+		// set isFetching to false.
+		this.state = {
+			users: [],
+			isFetching: false
+		};
+	}
+
+	componentDidMount() {
+		// Before performing the fetch, set isFetching to true
+		this.setState({ isFetching: true });
+
+		// After component mounts, call the API to get the
+		// users, then update state which triggers re-render.
+		// Add a delay to the URL and reset isFetching upon
+		// completion of the request.
+		fetch("https://reqres.in/api/users?delay=3")
+			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					users: json.data,
+					isFetching: false
+				});
+			});
+	}
+
 	render() {
+		const { users, isFetching } = this.state;
 		return (
 			<div className="App">
 				<JumbotronFluid
 					heading="User CRUD"
 					lead="Using an API for User CRUD operations"
 				/>
-				<UserList />
+				<UserList users={users} isFetching={isFetching} />
 			</div>
 		);
 	}
--- src/Component/UserList.js
+++ src/Component/UserList.js
@@ -2,53 +2,22 @@ import React, { Component } from "react";
 //import logo from "../logo.svg";
 import UserCard from "./UserCard";
 
-class UserList extends Component {
-  constructor() {
-    super();
+// class UserList extends Component {
+const UserList = ({ users, isFetching }) => {
+  // Generate the UserCard for each user
+  const userList = users.map(user => <UserCard user={user} key={user.id} />);
 
-    // Initialize users in state as an empty array and
-    // set isFetching to false.
-    this.state = {
-      users: [],
-      isFetching: false
-    };
-  }
-
-  componentDidMount() {
-    // Before performing the fetch, set isFetching to true
-    this.setState({ isFetching: true });
-
-    // After component mounts, call the API to get the
-    // users, then update state which triggers re-render.
-    // Add a delay to the URL and reset isFetching upon
-    // completion of the request.
-    fetch("https://reqres.in/api/users?delay=3")
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          users: json.data,
-          isFetching: false
-        });
-      });
-  }
-
-  render() {
-    const { users, isFetching } = this.state;
-    // Generate the UserCard for each user
-    const userList = users.map(user => <UserCard user={user} key={user.id} />);
-
-    // card-group is the layout wrapper for Bootstrap
-    // 4 cards. Add ternary operator to conditionally
-    // show Loading... if in the process of fetching.
-    return (
-      <div className="container">
-        <h1>User List</h1>
-        <div className="card-group">
-          {isFetching ? <p>Loading...</p> : userList}
-        </div>
+  // card-group is the layout wrapper for Bootstrap
+  // 4 cards. Add ternary operator to conditionally
+  // show Loading... if in the process of fetching.
+  return (
+    <div className="container">
+      <h1>User List</h1>
+      <div className="card-group">
+        {isFetching ? <p>Loading...</p> : userList}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserList;
